9020ff7c6a67e515a0a6023c87bf74b2218ce71f
Volodymyr Syzov <sizov.v.f@gmail.com>
2019-11-26 14:54:29 +0000

    fix: add spotlight to launcher (ARTP-867)

--- src/client/src/apps/SimpleLauncher/Launcher.tsx
+++ src/client/src/apps/SimpleLauncher/Launcher.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
 import { rules } from 'rt-styleguide'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,11 +7,16 @@ import { appConfigs } from './applicationConfigurations'
 import { LaunchButton } from './LaunchButton'
 import { LogoIcon } from 'rt-components'
 import { createGlobalStyle } from 'styled-components'
-import { ThemeStorageSwitch, styled } from 'rt-theme'
+import { styled, ThemeStorageSwitch } from 'rt-theme'
 import { open } from './tools'
 import { getOpenFinPlatform } from 'rt-platforms'
+import { Bounds } from 'openfin/_v2/shapes'
+import SearchIcon from './icons/searchIcon'
 
 library.add(faSignOutAlt)
+
+const INPUT_HEIGHT = 40
+const SEARCH_RESULT_HEIGHT = 30
 
 const exitHandler = async () => {
   const { OpenFin } = await getOpenFinPlatform()
@@ -36,41 +41,165 @@ const LauncherExit = () => (
     </LaunchButton>
   </ButtonContainer>
 )
-export class Launcher extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <LauncherGlobalStyle />
-        <Root>
-          <LogoContainer>
-            <LogoIcon width={1.3} height={1.3} />
-          </LogoContainer>
-          {appConfigs.map(app => (
-            <ButtonContainer key={app.name}>
-              <LaunchButton onClick={() => open(app)}>
-                {app.icon}
-                <IconTitle>{app.name}</IconTitle>
-              </LaunchButton>
-            </ButtonContainer>
-          ))}
-          <LauncherExit />
-          <ThemeSwitchContainer>
-            <ThemeStorageSwitch />
-          </ThemeSwitchContainer>
-        </Root>
-      </React.Fragment>
-    )
-  }
+
+async function animateWindowSize(bounds: Bounds, duration: number = 200) {
+  const window = await fin.Window.getCurrent()
+  return window.animate(
+    {
+      size: {
+        duration,
+        height: bounds.height,
+        width: bounds.width,
+      },
+    },
+    {
+      tween: 'ease-in-out',
+      interrupt: true,
+    },
+  )
 }
 
-const Root = styled.div`
+// async function changeWindowHeight(bounds: Bounds) {
+//   const window = await fin.Window.getCurrent()
+//   return window.resizeTo(
+//     bounds.width,
+//     bounds.height,
+//     'top-left'
+//   )
+// }
+
+const getWindowBounds = async () => {
+  const window = await fin.Window.getCurrent()
+  return window.getBounds()
+}
+
+const SEARCH_RESULTS = [
+  'Random Phrase and Idiom Generator',
+  'There will be times when',
+  'you may need',
+  'more than a random',
+  'word for what you want',
+  'to accomplish, and this',
+  'free online tool can help',
+  'The use of this tool',
+  'is quite simple. All',
+  'you need to do is indicate',
+  'the number of random phrases you',
+  'like to be displayed, and',
+  'then hit the "Generate Random',
+]
+
+function getRandomSearchResults(count: number) {
+  const result = []
+  for (let i = 0; i < count; i++) {
+    const index = Math.round(Math.random() * (SEARCH_RESULTS.length - 1))
+    result.push(SEARCH_RESULTS[index])
+  }
+  return result
+}
+
+export const Launcher: React.FC = () => {
+  const [initialBounds, setInitialBounds] = useState<Bounds>()
+  const [dummySearchResults, setDummySearchResults] = useState<ReadonlyArray<string>>([])
+  const [isSearchVisible, setIsSearchVisible] = useState(false)
+  const searchInput = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    getWindowBounds().then(setInitialBounds)
+  }, [])
+
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        if (!isSearchVisible || !initialBounds) {
+          return
+        }
+        setIsSearchVisible(false)
+        animateWindowSize(initialBounds)
+      }
+    }
+    document.addEventListener('keydown', onKeyDown)
+    return () => document.removeEventListener('keydown', onKeyDown)
+  }, [initialBounds, isSearchVisible])
+
+  const showSearch = async () => {
+    if (!initialBounds) {
+      return
+    }
+    if (!isSearchVisible) {
+      await animateWindowSize({ ...initialBounds, height: initialBounds.height + INPUT_HEIGHT })
+      setIsSearchVisible(true)
+    }
+    searchInput.current && searchInput.current.focus()
+  }
+
+  const onInputChange = (event: ChangeEvent) => {
+    const symbolsCount = (event.target as HTMLInputElement).value
+    if (!isSearchVisible || !initialBounds) {
+      return
+    }
+    const searchResultCount = symbolsCount.length === 0 ? 0 : Math.round(Math.random() * 10)
+    setDummySearchResults(getRandomSearchResults(searchResultCount))
+    animateWindowSize(
+      {
+        ...initialBounds,
+        height: initialBounds.height + INPUT_HEIGHT + searchResultCount * SEARCH_RESULT_HEIGHT,
+      },
+      75,
+    )
+  }
+
+  const Spotlight = () => (
+    <ButtonContainer>
+      <LaunchButton onClick={showSearch}>{SearchIcon}</LaunchButton>
+    </ButtonContainer>
+  )
+
+  return (
+    <RootContainer>
+      <LauncherGlobalStyle />
+      <HorizontalContainer>
+        <LogoContainer>
+          <LogoIcon width={1.3} height={1.3} />
+        </LogoContainer>
+        <Spotlight />
+        {appConfigs.map(app => (
+          <ButtonContainer key={app.name}>
+            <LaunchButton onClick={() => open(app)}>
+              {app.icon}
+              <IconTitle>{app.name}</IconTitle>
+            </LaunchButton>
+          </ButtonContainer>
+        ))}
+        <LauncherExit />
+        <ThemeSwitchContainer>
+          <ThemeStorageSwitch />
+        </ThemeSwitchContainer>
+      </HorizontalContainer>
+
+      <Input ref={searchInput} onChange={onInputChange} />
+
+      {dummySearchResults.map(searchResult => (
+        <SearchResult>{searchResult}</SearchResult>
+      ))}
+    </RootContainer>
+  )
+}
+
+const RootContainer = styled.div`
   height: 100%;
+  width: 100%;
+  background-color: ${({ theme }) => theme.core.lightBackground};
+  overflow: hidden;
+  color: ${({ theme }) => theme.core.textColor};
+`
+
+const HorizontalContainer = styled.div`
+  height: 52px;
   width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme }) => theme.core.lightBackground};
-  color: ${({ theme }) => theme.core.textColor};
 `
 
 const IconTitle = styled.span`
@@ -116,4 +245,28 @@ const LogoContainer = styled(IconContainer)`
     fill: ${({ theme }) => theme.core.textColor};
   }
   ${rules.appRegionDrag};
+`
+
+const Input = styled.input`
+  padding: 8px;
+  width: 100%;
+  height: ${INPUT_HEIGHT}px;
+  background: none;
+  outline: none;
+  border: none;
+  font-size: 1.25rem;
+  ${rules.appRegionNoDrag};
+`
+
+const SearchResult = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  height: ${SEARCH_RESULT_HEIGHT}px;
+  background: none;
+  outline: none;
+  border: none;
+  font-size: 1rem;
+  font-style: italic;
+  opacity: 0.75;
+  ${rules.appRegionNoDrag};
 `
