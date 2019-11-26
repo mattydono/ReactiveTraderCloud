0c2280dd8f6f47ec06ae3207e010ceeaebfddc45
Volodymyr Syzov <sizov.v.f@gmail.com>
2019-11-26 15:21:27 +0000

    fix: refactoring launcher (ARTP-867)

--- src/client/src/apps/SimpleLauncher/Launcher.tsx
+++ src/client/src/apps/SimpleLauncher/Launcher.tsx
@@ -1,22 +1,29 @@
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
-import { rules } from 'rt-styleguide'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
 import { appConfigs } from './applicationConfigurations'
 import { LaunchButton } from './LaunchButton'
 import { LogoIcon } from 'rt-components'
-import { createGlobalStyle } from 'styled-components'
-import { styled, ThemeStorageSwitch } from 'rt-theme'
+import { ThemeStorageSwitch } from 'rt-theme'
 import { open } from './tools'
 import { getOpenFinPlatform } from 'rt-platforms'
 import { Bounds } from 'openfin/_v2/shapes'
 import SearchIcon from './icons/searchIcon'
+import {
+  LauncherGlobalStyle,
+  ButtonContainer,
+  IconTitle,
+  RootContainer,
+  ThemeSwitchContainer,
+  HorizontalContainer,
+  Input,
+  LogoContainer,
+  SearchResult, INPUT_HEIGHT, SEARCH_RESULT_HEIGHT
+} from './launcherStyles'
+import { animateCurrentWindowSize, getCurrentWindowBounds } from './windowUtils';
 
 library.add(faSignOutAlt)
-
-const INPUT_HEIGHT = 40
-const SEARCH_RESULT_HEIGHT = 30
 
 const exitHandler = async () => {
   const { OpenFin } = await getOpenFinPlatform()
@@ -24,54 +31,14 @@ const exitHandler = async () => {
   return platform.window.close()
 }
 
-const LauncherGlobalStyle = createGlobalStyle`
-:root, body {
-  @media all {
-    font-size: 16px;
-    -webkit-app-region: drag;
-  }
-}
-`
-
 const LauncherExit = () => (
   <ButtonContainer key="exit">
     <LaunchButton onClick={exitHandler}>
-      <FontAwesomeIcon icon="sign-out-alt" />
+      <FontAwesomeIcon icon="sign-out-alt"/>
       <IconTitle>Exit</IconTitle>
     </LaunchButton>
   </ButtonContainer>
 )
-
-async function animateWindowSize(bounds: Bounds, duration: number = 200) {
-  const window = await fin.Window.getCurrent()
-  return window.animate(
-    {
-      size: {
-        duration,
-        height: bounds.height,
-        width: bounds.width,
-      },
-    },
-    {
-      tween: 'ease-in-out',
-      interrupt: true,
-    },
-  )
-}
-
-// async function changeWindowHeight(bounds: Bounds) {
-//   const window = await fin.Window.getCurrent()
-//   return window.resizeTo(
-//     bounds.width,
-//     bounds.height,
-//     'top-left'
-//   )
-// }
-
-const getWindowBounds = async () => {
-  const window = await fin.Window.getCurrent()
-  return window.getBounds()
-}
 
 const SEARCH_RESULTS = [
   'Random Phrase and Idiom Generator',
@@ -105,7 +72,7 @@ export const Launcher: React.FC = () => {
   const searchInput = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
-    getWindowBounds().then(setInitialBounds)
+    getCurrentWindowBounds().then(setInitialBounds)
   }, [])
 
   useEffect(() => {
@@ -115,7 +82,7 @@ export const Launcher: React.FC = () => {
           return
         }
         setIsSearchVisible(false)
-        animateWindowSize(initialBounds)
+        animateCurrentWindowSize(initialBounds)
       }
     }
     document.addEventListener('keydown', onKeyDown)
@@ -127,7 +94,7 @@ export const Launcher: React.FC = () => {
       return
     }
     if (!isSearchVisible) {
-      await animateWindowSize({ ...initialBounds, height: initialBounds.height + INPUT_HEIGHT })
+      await animateCurrentWindowSize({ ...initialBounds, height: initialBounds.height + INPUT_HEIGHT })
       setIsSearchVisible(true)
     }
     searchInput.current && searchInput.current.focus()
@@ -140,7 +107,7 @@ export const Launcher: React.FC = () => {
     }
     const searchResultCount = symbolsCount.length === 0 ? 0 : Math.round(Math.random() * 10)
     setDummySearchResults(getRandomSearchResults(searchResultCount))
-    animateWindowSize(
+    animateCurrentWindowSize(
       {
         ...initialBounds,
         height: initialBounds.height + INPUT_HEIGHT + searchResultCount * SEARCH_RESULT_HEIGHT,
@@ -157,12 +124,12 @@ export const Launcher: React.FC = () => {
 
   return (
     <RootContainer>
-      <LauncherGlobalStyle />
+      <LauncherGlobalStyle/>
       <HorizontalContainer>
         <LogoContainer>
-          <LogoIcon width={1.3} height={1.3} />
+          <LogoIcon width={1.3} height={1.3}/>
         </LogoContainer>
-        <Spotlight />
+        <Spotlight/>
         {appConfigs.map(app => (
           <ButtonContainer key={app.name}>
             <LaunchButton onClick={() => open(app)}>
@@ -171,13 +138,13 @@ export const Launcher: React.FC = () => {
             </LaunchButton>
           </ButtonContainer>
         ))}
-        <LauncherExit />
+        <LauncherExit/>
         <ThemeSwitchContainer>
-          <ThemeStorageSwitch />
+          <ThemeStorageSwitch/>
         </ThemeSwitchContainer>
       </HorizontalContainer>
 
-      <Input ref={searchInput} onChange={onInputChange} />
+      <Input ref={searchInput} onChange={onInputChange}/>
 
       {dummySearchResults.map(searchResult => (
         <SearchResult>{searchResult}</SearchResult>
@@ -185,88 +152,3 @@ export const Launcher: React.FC = () => {
     </RootContainer>
   )
 }
-
-const RootContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  background-color: ${({ theme }) => theme.core.lightBackground};
-  overflow: hidden;
-  color: ${({ theme }) => theme.core.textColor};
-`
-
-const HorizontalContainer = styled.div`
-  height: 52px;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
-const IconTitle = styled.span`
-  position: absolute;
-  bottom: 2px;
-  right: 0;
-  left: 0;
-  text-align: center;
-  font-size: 9px;
-  font-family: Lato;
-  color: transparent;
-  transition: color 0.3s ease;
-
-  /* avoids text highlighting on icon titles */
-  user-select: none;
-`
-
-const IconContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  &:hover {
-    span {
-      color: ${({ theme }) => theme.core.textColor};
-    }
-  }
-`
-
-const ButtonContainer = styled(IconContainer)`
-  ${rules.appRegionNoDrag};
-`
-const ThemeSwitchContainer = styled(ButtonContainer)`
-  width: 35%;
-`
-
-const LogoContainer = styled(IconContainer)`
-  width: 50%;
-  background-color: ${({ theme }) => theme.core.lightBackground};
-  .svg-icon {
-    fill: ${({ theme }) => theme.core.textColor};
-  }
-  ${rules.appRegionDrag};
-`
-
-const Input = styled.input`
-  padding: 8px;
-  width: 100%;
-  height: ${INPUT_HEIGHT}px;
-  background: none;
-  outline: none;
-  border: none;
-  font-size: 1.25rem;
-  ${rules.appRegionNoDrag};
-`
-
-const SearchResult = styled.div`
-  padding-left: 20px;
-  padding-right: 20px;
-  height: ${SEARCH_RESULT_HEIGHT}px;
-  background: none;
-  outline: none;
-  border: none;
-  font-size: 1rem;
-  font-style: italic;
-  opacity: 0.75;
-  ${rules.appRegionNoDrag};
-`
---
+++ src/client/src/apps/SimpleLauncher/launcherStyles.ts
@@ -0,0 +1,100 @@
+import { styled } from 'rt-theme';
+import { rules } from 'rt-styleguide';
+import { createGlobalStyle } from 'styled-components';
+
+export const INPUT_HEIGHT = 40
+export const SEARCH_RESULT_HEIGHT = 30
+
+export const LauncherGlobalStyle = createGlobalStyle`
+:root, body {
+  @media all {
+    font-size: 16px;
+    -webkit-app-region: drag;
+  }
+}
+`
+
+export const RootContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: ${({ theme }) => theme.core.lightBackground};
+  overflow: hidden;
+  color: ${({ theme }) => theme.core.textColor};
+`
+
+export const HorizontalContainer = styled.div`
+  height: 52px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const IconTitle = styled.span`
+  position: absolute;
+  bottom: 2px;
+  right: 0;
+  left: 0;
+  text-align: center;
+  font-size: 9px;
+  font-family: Lato;
+  color: transparent;
+  transition: color 0.3s ease;
+
+  /* avoids text highlighting on icon titles */
+  user-select: none;
+`
+
+export const IconContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  &:hover {
+    span {
+      color: ${({ theme }) => theme.core.textColor};
+    }
+  }
+`
+
+export const ButtonContainer = styled(IconContainer)`
+  ${rules.appRegionNoDrag};
+`
+export const ThemeSwitchContainer = styled(ButtonContainer)`
+  width: 35%;
+`
+
+export const LogoContainer = styled(IconContainer)`
+  width: 50%;
+  background-color: ${({ theme }) => theme.core.lightBackground};
+  .svg-icon {
+    fill: ${({ theme }) => theme.core.textColor};
+  }
+  ${rules.appRegionDrag};
+`
+
+export const Input = styled.input`
+  padding: 8px;
+  width: 100%;
+  height: ${INPUT_HEIGHT}px;
+  background: none;
+  outline: none;
+  border: none;
+  font-size: 1.25rem;
+  ${rules.appRegionNoDrag};
+`
+
+export const SearchResult = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  height: ${SEARCH_RESULT_HEIGHT}px;
+  background: none;
+  outline: none;
+  border: none;
+  font-size: 1rem;
+  font-style: italic;
+  opacity: 0.75;
+  ${rules.appRegionNoDrag};
+`
