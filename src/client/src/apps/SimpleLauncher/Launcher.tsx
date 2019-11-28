6f9bc7b0b5664592723e9a69807a41acb4eacbbd
Volodymyr Syzov <sizov.v.f@gmail.com>
2019-11-28 17:51:01 +0000

    fix: refactoring inout controls

--- src/client/src/apps/SimpleLauncher/Launcher.tsx
+++ src/client/src/apps/SimpleLauncher/Launcher.tsx
@@ -12,7 +12,6 @@ import {
   ButtonContainer,
   HorizontalContainer,
   IconTitle,
-  INPUT_HEIGHT,
   LauncherGlobalStyle,
   LogoContainer,
   RootContainer,
@@ -20,7 +19,7 @@ import {
 } from './styles'
 import { animateCurrentWindowSize, closeCurrentWindow, getCurrentWindowBounds } from './windowUtils';
 import Measure, { ContentRect } from 'react-measure'
-import { SearchControls } from './SearchControls';
+import { SearchControls, SearchState } from './SearchControls';
 
 library.add(faSignOutAlt)
 
@@ -43,7 +42,7 @@ export const Launcher: React.FC = () => {
   const [initialBounds, setInitialBounds] = useState<Bounds>()
   const [isSearchVisible, setIsSearchVisible] = useState<boolean>()
   const [isSearchBusy, setIsSearchBusy] = useState<boolean>()
-  const searchInput = useRef<HTMLInputElement>(null)
+  const searchInputRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     getCurrentWindowBounds().then(setInitialBounds)
@@ -54,19 +53,44 @@ export const Launcher: React.FC = () => {
     []
   );
 
+  // if search is made visible - focus on it
   useEffect(
     () => {
-      if (setIsSearchVisible) {
-        searchInput.current && searchInput.current.focus()
+      if (isSearchVisible) {
+        searchInputRef.current && searchInputRef.current.focus()
       }
     },
-    [setIsSearchVisible]
+    [isSearchVisible]
+  )
+
+  // resize window is search was hidden
+  useEffect(() => {
+      if (!initialBounds) {
+        console.warn(`Cant resize window - 'initialBounds' is not set yet`)
+        return
+      }
+      if (!isSearchVisible) {
+        animateCurrentWindowSize(initialBounds)
+      }
+    },
+    [isSearchVisible, initialBounds]
+  )
+
+  // hide search if Escape is pressed
+  useEffect(
+    () => {
+      const onKeyDown = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+          setIsSearchVisible(false)
+        }
+      }
+      document.addEventListener('keydown', onKeyDown)
+      return () => document.removeEventListener('keydown', onKeyDown)
+    }, []
   )
 
   const handleSearchStateChange = useCallback(
-    (state:ActionSearchState) => {
-      setIsSearchBusy(state.typing || state.loading)
-    },
+    (state: SearchState) => setIsSearchBusy(state.typing || state.loading),
     []
   )
 
@@ -77,10 +101,10 @@ export const Launcher: React.FC = () => {
       }
       animateCurrentWindowSize({
         ...initialBounds,
-        height: initialBounds.height + INPUT_HEIGHT + 17 + (contentRect.bounds ? contentRect.bounds.height : 0)
+        height: initialBounds.height + (contentRect.bounds ? contentRect.bounds.height : 0)
       })
     },
-    []
+    [initialBounds]
   )
 
   return (
@@ -92,7 +116,7 @@ export const Launcher: React.FC = () => {
           <AdaptiveLoader size={24} speed={isSearchBusy ? 0.8 : 0} seperation={1.5} type="secondary"/>
           {/*<LogoIcon width={1.3} height={1.3}/>*/}
         </LogoContainer>
-        <SearchButton onClick={showSearch}/>>
+        <SearchButton onClick={showSearch}/>
         <LauncherApps/>
         <LauncherExit/>
         <ThemeSwitchContainer>
@@ -100,19 +124,22 @@ export const Launcher: React.FC = () => {
         </ThemeSwitchContainer>
       </HorizontalContainer>
 
-      {isSearchVisible && (
-        <Measure
-          bounds
-          onResize={handleSearchSizeChange}>
-          {
-            ({ measureRef }) => (
-              <SearchControls
-                ref={measureRef}
-                onStateChange={handleSearchStateChange}/>
-            )
-          }
-        </Measure>
-      )}}
+      {
+        isSearchVisible && (
+          <Measure
+            bounds
+            onResize={handleSearchSizeChange}>
+            {
+              ({ measureRef }) => (
+                <SearchControls
+                  ref={measureRef}
+                  searchInputRef={searchInputRef}
+                  onStateChange={handleSearchStateChange}/>
+              )
+            }
+          </Measure>
+        )
+      }
 
     </RootContainer>
   )
--- src/client/src/apps/SimpleLauncher/SearchControls.tsx
+++ src/client/src/apps/SimpleLauncher/SearchControls.tsx
@@ -2,191 +2,145 @@ import React, {
   ChangeEvent,
   FocusEventHandler,
   KeyboardEventHandler,
+  RefObject,
   useCallback,
   useEffect,
-  useReducer,
-  useRef,
   useState
 } from 'react'
 import throttle from 'lodash/throttle'
 import debounce from 'lodash/debounce'
-import { Bounds } from 'openfin/_v2/shapes'
-import { Input, INPUT_HEIGHT } from './styles'
-import { animateCurrentWindowSize, getCurrentWindowBounds } from './windowUtils';
+import { Input } from './styles'
 import { DetectIntentResponse } from 'dialogflow';
-import { take, tap, timeout } from 'rxjs/operators';
+import { take, timeout } from 'rxjs/operators';
 import { useServiceStub } from '../SpotlightRoute/context';
 import { getInlineSuggestionsComponent, Response } from './spotlight';
-import { initialState, launcherReducer } from './launcherReducer';
 import { usePlatform } from 'rt-platforms';
-import Measure, { ContentRect } from 'react-measure'
 import { handleIntent } from '../SpotlightRoute/handleIntent';
 
-export type
-
-export const SearchControls: React.FC = () => {
-  const [{ request, response, contacting, isTyping, isSearchVisible }, dispatch] = useReducer(launcherReducer, initialState)
-  const [initialBounds, setInitialBounds] = useState<Bounds>()
-  const searchInput = useRef<HTMLInputElement>(null)
-  const platform = usePlatform()
-
-  const serviceStub = useServiceStub()
-
-  useEffect(
-    () => {
-      onStateChange({
-        loading:contacting,
-        typing: isTyping
-      })
-    },
-    [contacting, isTyping, onStateChange]
-  )
-
-  useEffect(() => {
-    getCurrentWindowBounds().then(setInitialBounds)
-  }, [])
-
-  useEffect(() => {
-    if (!request) {
-      return
-    }
-    if (!serviceStub) {
-      console.error(`Error creating subscription - serviceStub was not defined`)
-      return
-    }
-    console.log('sending request', request)
-    dispatch({ type: 'SEND_REQUEST' })
-
-    const subscription = serviceStub
-      .createRequestResponseOperation<DetectIntentResponse[], string>(
-        'nlp-vlad',
-        'getNlpIntent',
-        request,
-      )
-      .pipe(
-        tap(() => console.info(JSON.stringify(request))),
-        timeout(10000),
-        take(1),
-      )
-      .subscribe(
-        response => {
-          dispatch({ type: 'RECEIVE_RESPONSE', response: response[0] })
-        },
-        (err: any) => {
-          console.error(err)
-          dispatch({ type: 'RECEIVE_RESPONSE' })
-        },
-      )
-
-    return () => {
-      subscription.unsubscribe()
-    }
-  }, [serviceStub, request])
-
-  const handleOnKeyDown: KeyboardEventHandler<HTMLInputElement> = useCallback(e => {
-    if (e.key === 'Enter' && response) {
-      handleIntent(response, platform)
-    }
-  }, [response, platform])
-
-  useEffect(() => {
-    const onKeyDown = (event: KeyboardEvent) => {
-      if (event.key === 'Escape') {
-        dispatch({ type: 'RECEIVE_RESPONSE' })
-        if (!isSearchVisible || !initialBounds) {
-          return
-        }
-        dispatch({ type: 'UPDATE_SEARCH_VISIBILITY', visible: false })
-        animateCurrentWindowSize(initialBounds)
-      }
-    }
-    document.addEventListener('keydown', onKeyDown)
-    return () => document.removeEventListener('keydown', onKeyDown)
-  }, [initialBounds, isSearchVisible])
-
-  const handleResponseSizeChange = (contentRect: ContentRect) => {
-    if (!initialBounds) {
-      return
-    }
-    animateCurrentWindowSize({
-      ...initialBounds,
-      height: initialBounds.height + INPUT_HEIGHT + 17 + (contentRect.bounds ? contentRect.bounds.height : 0)
-    })
-  }
-
-  const handleFocus: FocusEventHandler<HTMLInputElement> = useCallback(e => {
-    e.currentTarget.setSelectionRange(0, e.currentTarget.value.length)
-  }, [])
-
-  const throttledSendRequest = useCallback(
-    throttle(
-      (requestString: string) => dispatch({ type: 'SET_REQUEST', request: requestString }),
-      250,
-      {
-        leading: false,
-        trailing: true
-      }
-    ),
-    []
-  )
-
-  const debouncedStopTyping = useCallback(
-    debounce(
-      () => dispatch({ type: 'STOP_TYPING' }),
-      350,
-      {
-        leading: false,
-        trailing: true
-      }
-    ),
-    []
-  )
-
-  /**
-   * Sets start typing immediately and sets stop typing in timeout -
-   * this is to make sure that isTyping state is staying true for a bit after
-   * finishing typing (controls animation)
-   **/
-  const startTyping = useCallback(
-    () => {
-      dispatch({ type: 'START_TYPING' });
-      debouncedStopTyping()
-    },
-    []
-  )
-
-  const handleChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      startTyping()
-      const requestString = e.target.value
-      throttledSendRequest(requestString)
-    },
-    [throttledSendRequest, startTyping]
-  )
-
-  const inlineSuggestions = response && getInlineSuggestionsComponent(response, platform)
-
-  return (
-    <>
-      <Input
-        onChange={handleChange}
-        ref={searchInput}
-        onFocus={handleFocus}
-        onKeyDown={handleOnKeyDown}/>
-
-      {response && (
-        <Measure
-          bounds
-          onResize={handleResponseSizeChange}>
-          {
-            ({ measureRef }) => (
-              <Response ref={measureRef}>
-                {inlineSuggestions}
-              </Response>
-            )
-          }
-        </Measure>
-      )}
-    </>
-  )
+export type SearchState = {
+  loading: boolean,
+  typing: boolean
 }
+
+export interface SearchControlsProps {
+  onStateChange: (state: SearchState) => void
+  searchInputRef: RefObject<HTMLInputElement>
+}
+
+export const SearchControls = React.forwardRef<HTMLDivElement, SearchControlsProps>(
+  ({ onStateChange, searchInputRef }, ref) => {
+    const [isTyping, setIsTyping] = useState(false)
+    const [response, setResponse] = useState<DetectIntentResponse>()
+    const [contacting, setContacting] = useState(false)
+
+    const platform = usePlatform()
+    const serviceStub = useServiceStub()
+
+    useEffect(
+      () => {
+        onStateChange({
+          loading: contacting,
+          typing: isTyping
+        })
+      },
+      [contacting, isTyping, onStateChange]
+    )
+
+    const sendRequest = useCallback((requestString: string) => {
+      if (!requestString) {
+        console.warn(`Skipping sending the request - request string is empty`)
+        return
+      }
+      if (!serviceStub) {
+        console.error(`Error creating subscription - serviceStub was not defined`)
+        return
+      }
+      console.info('Sending NLP request:', requestString)
+
+      setContacting(true)
+
+      const subscription = serviceStub
+        .createRequestResponseOperation<DetectIntentResponse[], string>(
+          'nlp-vlad',
+          'getNlpIntent',
+          requestString,
+        )
+        .pipe(
+          timeout(10000),
+          take(1),
+        )
+        .subscribe(
+          result => {
+            setContacting(false)
+            setResponse(result[0])
+          },
+          (err: any) => {
+            console.error(`Error in NLP request: ${err}`)
+            setContacting(false)
+          },
+        )
+
+      return () => subscription && subscription.unsubscribe()
+    }, [serviceStub])
+
+    const handleOnKeyDown: KeyboardEventHandler<HTMLInputElement> = useCallback(e => {
+      if (e.key === 'Enter' && response) {
+        handleIntent(response, platform)
+      }
+    }, [response, platform])
+
+    const handleFocus: FocusEventHandler<HTMLInputElement> = useCallback(e => {
+      e.currentTarget.setSelectionRange(0, e.currentTarget.value.length)
+    }, [])
+
+    const throttledSendRequest = useCallback(
+      throttle(
+        (requestString: string) => sendRequest(requestString),
+        250,
+        {
+          leading: false,
+          trailing: true
+        }
+      ),
+      []
+    )
+
+    const debouncedStopTyping = useCallback(
+      debounce(
+        () => setIsTyping(false),
+        350,
+        {
+          leading: false,
+          trailing: true
+        }
+      ),
+      []
+    )
+
+    const handleChange = useCallback(
+      (e: ChangeEvent<HTMLInputElement>) => {
+        setIsTyping(true)
+        debouncedStopTyping()
+        throttledSendRequest(e.target.value)
+      },
+      [throttledSendRequest, debouncedStopTyping]
+    )
+
+    const inlineSuggestions = response && getInlineSuggestionsComponent(response, platform)
+
+    return (
+      <div ref={ref}>
+        <Input
+          onChange={handleChange}
+          ref={searchInputRef}
+          onFocus={handleFocus}
+          onKeyDown={handleOnKeyDown}/>
+
+        {response && (
+          <Response>
+            {inlineSuggestions}
+          </Response>
+        )}
+      </div>
+    )
+  })
